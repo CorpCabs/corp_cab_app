23793ccb6f71ebdf73c70129b0bfe2994270bc96
pnaskardev <pnaskardev@gmail.com>
2024-12-15 10:11:13 +0530

    added some changes

--- lib/feature/Home/view/home.dart
+++ lib/feature/Home/view/home.dart
@@ -1,18 +1,11 @@
-import 'package:auto_route/auto_route.dart';
-import 'package:corp_cab_app/app/common/commonTextField.dart';
-import 'package:corp_cab_app/app/common/toast.dart';
-import 'package:corp_cab_app/app/providers/cab_booking_provider.dart';
-import 'package:corp_cab_app/app/validators/text_field_validator.dart';
 import 'package:corp_cab_app/core/extensions/context_extensions.dart';
-import 'package:corp_cab_app/core/utils/logger/logger_utils.dart';
-import 'package:corp_cab_app/feature/ScaffoldWithNavbar/components/vehicleCard.dart';
-import 'package:corp_cab_app/feature/book_ride/bookride.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
-import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,13 +14,12 @@ class HomePage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               const SizedBox(height: 20),
-              Align(
+              const Align(
                 alignment: Alignment.centerLeft,
                 child: Text(
-                  "GOOD DAY,\nSiffat",
+                  'GOOD DAY,\nSiffat',
                   style: TextStyle(
                     fontSize: 32,
                     fontWeight: FontWeight.bold,
@@ -54,7 +46,7 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
                 child: const Text(
-                  "Book a ride now",
+                  'Book a ride now',
                   style: TextStyle(fontSize: 18),
                 ),
               ),
@@ -70,7 +62,7 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
                 child: const Text(
-                  "Schedule for Later",
+                  'Schedule for Later',
                   style: TextStyle(fontSize: 18, color: Colors.black),
                 ),
               ),
