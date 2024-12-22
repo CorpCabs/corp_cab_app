57ec7b18837b3dc656e342fac9f2caba5691e446
thehappybaloney <a.chatterjeeofficial2003@gmail.com>
2024-12-23 00:58:57 +0530

    Fix: Removed Run App from splash

--- lib/feature/splash_screen/splash.dart
+++ lib/feature/splash_screen/splash.dart
@@ -3,10 +3,6 @@
 // import 'package:flutter/material.dart';
 // import 'package:go_router/go_router.dart';
 
-// void main() {
-//   runApp(MyApp());
-// }
-
 // class MyApp extends StatelessWidget {
 //   @override
 //   Widget build(BuildContext context) {
